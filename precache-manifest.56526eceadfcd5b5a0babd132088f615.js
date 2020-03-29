6f79e89ed13bf8ab2143a3c86ac44701180e2071
Kaan <kkureli@gmail.com>
2020-03-29 14:18:15 +0300

    Updates

---
+++ precache-manifest.56526eceadfcd5b5a0babd132088f615.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71e4c17add0442955bb79de9b7f3da63",
+    "url": "/Calculator-React/index.html"
+  },
+  {
+    "revision": "8f3ef486743d2423c1de",
+    "url": "/Calculator-React/static/css/main.8bad40fd.chunk.css"
+  },
+  {
+    "revision": "5f1bb4550b03aadef087",
+    "url": "/Calculator-React/static/js/2.8c49264e.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/Calculator-React/static/js/2.8c49264e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3ef486743d2423c1de",
+    "url": "/Calculator-React/static/js/main.eab9f00a.chunk.js"
+  },
+  {
+    "revision": "3aed76b67230fe2dfbc0",
+    "url": "/Calculator-React/static/js/runtime-main.db4336de.js"
+  }
+]);
